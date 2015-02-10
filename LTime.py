3067702ba1956bbc56ab24283a236a2b705659ee
jd_7773da56ae306 <635333273@qq.com>
2015-02-10 12:34:21 +0800

    Init

---
+++ LTime.py
@@ -0,0 +1,5 @@
+import os
+import webbrowser
+os.system('"C:/Program Files/Google/Chrome/Application/chrome" www.zhihu.com')
+os.system('"C:/Program Files/Google/Chrome/Application/chrome" www.huaban.com')
+os.system('"C:/Program Files/Google/Chrome/Application/chrome" https://500px.com')
